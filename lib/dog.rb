a176ed571ac08a21a35f7ab6432f992034605d9e
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-12-27 18:51:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+class Dog 
+  attr_accessor :name 
+  @@all = []
+  def save 
+    @@all << self
+  end
+  def initialize(name)
+    @name = name 
+    save 
+  end 
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end 
+  end 
+  def self.clear_all
+    @@all.clear
+  end
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+end 
